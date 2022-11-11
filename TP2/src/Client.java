d09bec9fa9f83f10a0a8ce5444a04107a9259817
150Pocahontas <tixa20004@gmail.com>
2022-11-11 22:52:15 +0000

    Server-Client connection

--- TP2/src/Client.java
+++ TP2/src/Client.java
@@ -2,19 +2,24 @@ import java.net.*;
 import java.io.*;
 
 class Client{
+    // The client program reciveis as arguments: hostaddress:port , NAME , TYPE VALUE , FLAG[optional] 
     public static void main(String[] args) throws Exception {
-        // Create a socket to connect to the server
-        Socket clientSocket = new Socket("10.2.2.1", 1234);
-        System.out.println("Connected to server");
-        //get input from user   
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter a message: ");
-        String message = br.readLine();
-        //send the message to the server
-        OutputStream os = clientSocket.getOutputStream();
-        DataOutputStream dos = new DataOutputStream(os);
-        dos.writeUTF(message);    
-        // Close the socket
-        clientSocket.close();
+        try { 
+            //split sting into host and port
+            String[] hostPort = args[0].split(":");
+            //create a socket to connect to the server
+            Socket clientSocket = new Socket(hostPort[0], Integer.parseInt(hostPort[1]));
+            System.out.println("Connected to server");
+            //get input from user   
+            String message = "query";
+            //send the message to the server
+            OutputStream os = clientSocket.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(os);
+            dos.writeUTF(message);    
+            // Close the socket
+            clientSocket.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
     }
 }
---
+++ TP2/src/Query.java
@@ -0,0 +1,8 @@
+
+class Query{
+    public static void main(String[] args) {
+        //DNS message   
+        
+
+    }
+}
