8a6ca2ef6be67a2e1b9fb2b0d8e9d246f826543f
46siri <meriam.kh77@gmail.com>
2022-12-28 22:40:04 +0000

    responseServer

--- TP2/src/ResponseServer.java
+++ TP2/src/ResponseServer.java
@@ -17,27 +17,41 @@ public class ResponseServer implements Runnable {
 
     public void run() {
            
-        byte[] messageDecrypted = DNSmessage.decrypt(dnsMessage);
-        DNSmessage message = new DNSmessage(messageDecrypted);
-
-        String[] name = (message.getName()).split("\\.");
-        DNSmessage response = null;
-        String dbName = name[name.length-2] + "." + name[name.length-1];
-        String type = message.getTypeOfValue();
-        if(db.containsKey(dbName)){
-            response = new DNSmessage(message.getId(), message.getFlags(), 0, db.get(dbName).getNumRV(type), db.get(dbName).getNumAV(type), db.get(dbName).getNumExtra(), message.getName(), message.getTypeOfValue(), db.get(dbName).getResponseValues(type) ,db.get(dbName).getAuthoritativeValues(type), db.get(dbName).getExtraValues());
-        }else{
-                    response = new DNSmessage(message.getId(), message.getFlags(), 2, 0, 0, 0, message.getName(), message.getTypeOfValue(), null,null, null);
-                }
-
-            byte[] messageEncrypted = DNSmessage.encrypt(dnsMessage.toByteArray());
-                //send the message to the client
-                System.out.println("Sending message to client");
-                InetAddress clientAddress = datagramPacket.getAddress();
-                int clientPort = datagramPacket.getPort();
-                datagramPacket = new DatagramPacket(messageEncrypted, messageEncrypted.length, clientAddress, clientPort);
-                datagramSocket.send(datagramPacket);
+        byte[] messageDecrypted;
+        try {
             
+            messageDecrypted = DNSmessage.decrypt(dnsMessage);
+            DNSmessage message = new DNSmessage(messageDecrypted);
+            String name[] = (message.getName()).split("\\.");
+            DNSmessage response = null;
+            String dbName = name[name.length-2] + "." + name[name.length-1];
+            String type = message.getTypeOfValue();
+            if(Server.getBDName(dbList, message.getName()) != null){
+                ParseDBFile db = new ParseDBFile(Server.getBDName(dbList, message.getName()));
+                db.parseFile();
+                response = new DNSmessage(message.getId(), message.getFlags(), 0, db.getNumRV(type), db.getNumAV(type), db.getNumExtra(), message.getName(), message.getTypeOfValue(), db.getResponseValues(type) ,db.getAuthoritativeValues(type), db.getExtraValues());
+            }else{
+                response = new DNSmessage(message.getId(), message.getFlags(), 2, 0, 0, 0, message.getName(), message.getTypeOfValue(), null,null, null);
+            }
+             // encrypt message
+            byte[] messageEncrypted = DNSmessage.encrypt(response.toByteArray());
+             //send the message to the client
+            System.out.println("Sending message to client");
+            InetAddress clientAddress = datagramPacket.getAddress();
+            int clientPort = datagramPacket.getPort();
+            datagramPacket = new DatagramPacket(messageEncrypted, messageEncrypted.length, clientAddress, clientPort);
+            DatagramSocket datagramSocket = new DatagramSocket(datagramPacket.getPort());
+            datagramSocket.send(datagramPacket);
             System.out.println("Message sent to client");
+
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+
+        
+        
     }
 }
